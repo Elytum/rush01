92dd70b0c92f18637232e2d010813c9c04e8409d
Arthur CHAZAL <achazal@e2r2p19.42.fr>
2016-01-04 13:12:19 +0100

    Start of users

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+function User(socket) {
+  this.socket = socket;
+}
+
+var users = {};
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	users[socket.id] = User(socket);
 	console.log('User '+socket.id+' logged in');
 
 	socket.on('msg', function(msg){
@@ -14,11 +21,24 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('key', function(key){
-		console.log('User '+socket.id+' pressed '+String.fromCharCode(key));
+		key = String.fromCharCode(key);
+		if (key == 'w')
+			io.emit('move', 'Used '+socket.id+' moved up');
+		else if (key == 's')
+			io.emit('move', 'Used '+socket.id+' moved down');
+		else if (key == 'a')
+			io.emit('move', 'Used '+socket.id+' moved left');
+		else if (key == 'd')
+			io.emit('move', 'Used '+socket.id+' moved right');
 	});
 
 	socket.on('disconnect', function() {
+		delete users[socket.id];
 		console.log('User '+socket.id+' logged out');
+	});
+
+	socket.on('list', function(msg){
+		console.log(users);
 	});
 });
 
