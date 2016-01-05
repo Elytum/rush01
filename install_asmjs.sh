bd4ae78cc22dbe7679bf7ee519773069f293df12
Arthur CHAZAL <achazal@e2r1p11.42.fr>
2016-01-05 16:25:14 +0100

    Fuckit, go for open

--- install_asmjs.sh
+++ install_asmjs.sh
@@ -1,15 +1,17 @@
 # Download Emscripten
 curl https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz > emsdk-portable.tar.gz
 # Unzip the downloaded file here
-tar -zxvf emsdk-portable.tar.gz
+open emsdk-portable.tar.gz
+# Go in Emscripten directory
+cd emsdk-portable
 # Fetch the latest registry of available tools.
-./emsdk_portable/emsdk update
+./emsdk update
 # Download and install the latest SDK tools.
-./emsdk_portable/emsdk install latest
+./emsdk install latest
 # Make the "latest" SDK "active"
-./emsdk_portable/emsdk activate latest
+./emsdk activate latest
 # Set the current Emscripten path on Linux/Mac OS X
-source ./emsdk_portable/emsdk_env.sh
+source ./emsdk_env.sh
 
 # To compile into asm.js file, just run "./emcc -O1 -s ASM_JS=1 ./file.cpp"
 # To run it, just run "node a.out.js"
