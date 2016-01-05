321c39ea83fdd6a27559662f784c8c86de9b884f
Arthur CHAZAL <achazal@e2r1p11.42.fr>
2016-01-05 15:59:40 +0100

    Update

--- install_asmjs.sh
+++ install_asmjs.sh
@@ -1,16 +1,12 @@
 # Download Emscripten
-curl https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-1.35.0-portable-64bit.zip > emsdk-1.35.0-portable-64bit.zip
-# Create extraction directory for Emscripten
-mkdir emsdk-1.35.0-portable-64bit
-# Go in it
-cd emsdk-1.35.0-portable-64bit
+url https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz > emsdk-portable.tar.gz
 # Unzip the downloaded file here
-unzip ../https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-1.35.0-portable-64bit.zip
+tar -zxvf emsdk-portable.tar.gz
 # Fetch the latest registry of available tools.
-./emsdk update
+./emsdk_portable/emsdk update
 # Download and install the latest SDK tools.
-./emsdk install latest
+./emsdk_portable/emsdk install latest
 # Make the "latest" SDK "active"
-./emsdk activate latest
+./emsdk_portable/emsdk activate latest
 # Set the current Emscripten path on Linux/Mac OS X
-source ./emsdk_env.sh
+source ./emsdk_portable/emsdk_env.sh
