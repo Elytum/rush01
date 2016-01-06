76c2db233a0800f9a602bc3e52f16350d8856699
Arthur CHAZAL <achazal@e2r1p13.42.fr>
2016-01-06 22:46:09 +0100

    Gotta go

--- install.js
+++ install.js
@@ -12,20 +12,38 @@ function launch_query(query) {
 }
 
 // launch_query('CREATE KEYSPACE game WITH REPLICATION = { \'class\' : \'SimpleStrategy\', \'replication_factor\' : 1 };');
-// launch_query('USE game;');
-// launch_query('CREATE TABLE users (  \
-//   uid varchar PRIMARY KEY,          \
-//   login varchar,                    \
-//   password varchar,                 \
-//   ip bigint,                        \
-//   sid bigint,                       \
-//   safe int,                         \
-//   socket bigint,                    \
-//   creation_date bigint              \
-// );');
 
 
-launch_query('SELECT login FROM users;');
+launch_query('USE game;');
+launch_query('CREATE TABLE users (  
+uid varchar PRIMARY KEY,          
+suscribed boolean PRIMARY KEY,
+login varchar,                    
+password varchar,                 
+ip bigint,                        
+sid bigint,                       
+safe boolean,                     
+socket bigint,                    
+creation_date bigint              
+);');
+
+										// launch_query('CREATE TABLE ips (      
+										// 	ip bigint PRIMARY KEY,           
+										// 	uids set<varchar>                 
+										// );');
+
+										// launch_query('SELECT login FROM users;');
+										// var uid = '42';
+										// var ip = 21;
+										// launch_query('UPDATE ips
+										//        SET ips = ips + {\''+uid+'\'} WHERE ip = '+ip+';');
+
+										// launch_query('INSERT INTO ips (ip, uids)
+										//        VALUES(21, {\'42\'});');
+
+// launch_query('UPDATE ips SET uids = uids + {\'63\'} WHERE ip = 21;');
+
+launch_query('SELECT count(*) FROM ips WHERE ip = 21;');
 
 // client.execute('SELECT name, email FROM game where key=?', ['mick-jagger'], function (err, result) {
 // 	if (err)
